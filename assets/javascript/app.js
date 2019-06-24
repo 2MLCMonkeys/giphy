155b56544543790fdc5dd27e0fb53b9c8505cd70
2mlcmonkeys <2mlcmonkeys@gmail.com>
2019-06-23 20:00:27 -0400

    adding more function, gif on and off click, styled returned datat position

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,6 @@ var buttonDiv = $("#buttons-container");
 var resultsDiv = $("#data-result");
 var userInput = $("#user-input");
 var submitButton = $("#user-submit");
-var pickedReaction = $(".reactions");
 
 
 //buttons displayed on load
@@ -22,7 +21,7 @@ $(document).ready(function () {
 });
 
 //on click drawing AJAX giphy query
-pickedReaction.on("click", function(){
+function pickedReaction(){
     var clickedReaction = $(this).attr("value");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +clickedReaction+ "&api_key=fV8e2q8TyrpgJ53QMy8id3VjWkURM40e&limit=10";
 
@@ -32,20 +31,30 @@ pickedReaction.on("click", function(){
     $.ajax({
         url: queryURL,
         method: "GET"
-    })
-    then(function(response){
+    }).then(function(response){
         console.log(response);
         var results = response.data;
         for(var j = 0; j < results.length; j++){
             if(results[j].rating !== "r" && results[j].rating !== "pg-13"){
-                var newDiv = $("<div>");
+                var newDiv = $("<div class='gif-contain'>");
                 newDiv.html(`<p>Rating: ${results[j].rating}</p>
-                <img src=" ${results[j].images.fixed_height.url}>`);
+                <img src=${results[j].images.fixed_height_still.url} data-still=${results[j].images.fixed_height_still.url} data-animate=${results[j].images.fixed_height.url} data-state="still" class="gif">`);
                 resultsDiv.prepend(newDiv);
-            }
+           }
         }
+        $(".gif").on("click", function() {
+            var state = $(this).attr("data-state");
+           
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
     });
-});
+};
 
 //creates all buttons in topics array
 function renewButtons() {
@@ -68,3 +77,6 @@ submitButton.on("click", function(event){
     topics.push(reaction);
     renewButtons();
 })
+
+//click listener to all dynamically created buttons
+$(document).on("click", ".reactions", pickedReaction);
